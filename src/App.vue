1a27ae0b544cd2d30feca3ea0d99f7494f18dd11
Mattari <ac.dev@hotmail.com>
2024-10-26 11:24:50 +0200

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header></header>
+
+  <main>
+    <h1>Github User Search App</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  flex-grow: 1;
+  display: grid;
+  place-items: center;
+}
+</style>
