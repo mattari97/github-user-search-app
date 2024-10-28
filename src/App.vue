b7acefbc2a56421a581b769e1ccf95ce9d252530
Mattari <ac.dev@hotmail.com>
2024-10-28 22:17:01 +0100

    Create sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import MainHeader from '@/components/MainHeader.vue'
+import MainHeader from '@/components/MainHeader.vue';
+import SearchBar from '@/components/SearchBar.vue';
 </script>
 
 <template>
   <MainHeader />
+  <SearchBar />
 </template>
 
 <style scoped></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import type { TSearchError } from '@/types';
+
+const error: Ref<TSearchError> = ref(null);
+const username: Ref<string> = ref('');
+const handleSearch = () => {
+  console.log(username.value);
+};
+</script>
+
+<template>
+  <section>
+    <input
+      v-model="username"
+      type="text"
+      placeholder="Search GitHub username..."
+    />
+    <span v-if="error">{{ error }}</span>
+    <button @click="handleSearch">Search</button>
+  </section>
+</template>
