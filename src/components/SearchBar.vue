941113bfb30394fcf37ae6c6a5622b504e78dbb8
Mattari <ac.dev@hotmail.com>
2024-11-02 22:57:46 +0100

    Use classes for performance

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -48,8 +48,8 @@ const clearError = () => {
 </script>
 
 <template>
-  <section>
-    <div>
+  <section class="search">
+    <div class="search__group">
       <SearchIcon />
       <input
         v-model.trim="username"
@@ -60,78 +60,101 @@ const clearError = () => {
         placeholder="Search username..."
       />
     </div>
-    <span v-if="searchError">{{ searchError }}</span>
-    <button @click="handleSearch" :aria-disabled="ariaDisabled">Search</button>
+    <span class="search__error" v-if="searchError">{{ searchError }}</span>
+    <button
+      class="search__submit"
+      @click="handleSearch"
+      :aria-disabled="ariaDisabled"
+    >
+      Search
+    </button>
   </section>
 </template>
 
-<style scoped>
-section {
+<style scoped lang="scss">
+.search {
   display: flex;
   position: relative;
   align-items: center;
   margin-block-start: 2rem;
   border-radius: 1rem;
   background-color: var(--clr-surface);
-}
+  box-shadow: var(--box-shadow);
 
-div {
-  flex: 1;
-  display: flex;
-  border-radius: 0.5rem;
-  margin-inline-start: 0.5rem;
-  padding-inline-start: 0.5rem;
-}
+  &__group {
+    flex: 1;
+    display: flex;
+    border-radius: 0.5rem;
+    margin-inline-start: 0.5rem;
+    padding-inline-start: 0.5rem;
 
-div:focus-within {
-  outline: 2px solid hsl(var(--clr-primary));
-}
+    &:focus-within {
+      outline: 2px solid hsl(var(--clr-primary));
+    }
 
-svg {
-  flex-shrink: 0;
-  color: hsl(var(--clr-primary));
-  width: 20px;
-}
+    & > svg {
+      flex-shrink: 0;
+      color: hsl(var(--clr-primary));
+      width: 20px;
 
-input {
-  flex: 1;
-  min-width: 0;
-  margin-inline: 0.5rem;
-  padding-block: 0.75rem;
-  flex-grow: 1;
-  font-size: var(--fs-sm);
-  color: hsl(var(--clr-text-200));
-}
+      @media (min-width: 39rem) {
+        width: 24px;
+      }
+    }
 
-input::placeholder {
-  color: var(--clr-text-100);
-}
+    & > input {
+      flex: 1;
+      min-width: 0;
+      margin-inline: 0.5rem;
+      padding-block: 0.75rem;
+      flex-grow: 1;
+      font-size: var(--fs-sm);
+      color: hsl(var(--clr-text-200));
 
-input:focus-visible {
-  outline: none;
-}
+      &::placeholder {
+        color: var(--clr-text-100);
+      }
 
-span {
-  position: absolute;
-  right: 0.5rem;
-  bottom: calc(100% + 2px);
-  color: hsl(var(--clr-error));
-  font-size: var(--fs-sm);
-}
+      &:focus-visible {
+        outline: none;
+      }
 
-button {
-  align-self: center;
-  border-radius: 0.5rem;
-  padding: 0.75rem 1rem;
-  margin: 0.5rem;
-  color: hsl(var(--clr-light-100));
-  background-color: hsl(var(--clr-primary));
-  font-weight: var(--fw-bold);
-  font-size: var(--fs-sm);
-}
+      @media (min-width: 39rem) {
+        margin-inline: 1rem;
+        font-size: var(--fs-base);
+      }
+    }
+  }
 
-button[aria-disabled='true'] {
-  background-color: hsl(var(--clr-primary) / 0.5);
-  cursor: not-allowed;
+  &__error {
+    position: absolute;
+    right: 0.5rem;
+    bottom: calc(100% + 2px);
+    color: hsl(var(--clr-error));
+    font-size: var(--fs-sm);
+  }
+
+  &__submit {
+    cursor: pointer;
+    align-self: center;
+    border-radius: 0.5rem;
+    padding: 0.75rem 1rem;
+    margin: 0.5rem;
+    color: hsl(var(--clr-light-100));
+    background-color: hsl(var(--clr-primary));
+    font-weight: var(--fw-bold);
+    font-size: var(--fs-sm);
+    transition: background-color var(--animation-duration) ease-out;
+
+    &:hover,
+    &:focus-visible {
+      background-color: hsl(var(--clr-primary--active));
+    }
+
+    &[aria-disabled='true'] {
+      background-color: hsl(var(--clr-primary) / 0.5);
+      cursor: not-allowed;
+    }
+  }
 }
 </style>
